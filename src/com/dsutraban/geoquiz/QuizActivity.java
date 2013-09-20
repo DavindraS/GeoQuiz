11dbc14aa14518f4867f4dd2c97b02a0142d9500
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-19 21:48:11 -0400

    Added onClickListeners to both buttons

--- src/com/dsutraban/geoquiz/QuizActivity.java
+++ src/com/dsutraban/geoquiz/QuizActivity.java
@@ -1,15 +1,39 @@
 package com.dsutraban.geoquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends Activity {
 
+	private Button mTrueButton;
+	private Button mFalseButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
